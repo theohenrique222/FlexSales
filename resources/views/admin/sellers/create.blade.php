5507b51c1621563531337102f0fe8e185193760e
Theo Henrique <theodoro222@hotmail.com>
2025-04-14 20:36:45 -0300

    upgrade in view register seller

--- resources/views/admin/sellers/create.blade.php
+++ resources/views/admin/sellers/create.blade.php
@@ -3,41 +3,42 @@
 @section('title', 'Cadastrar Vendedor')
 
 @section('content_header')
-    <h1>Cadastrar Vendedor</h1>
+    <h1><i class="fas fa-user-plus text-success"></i> Cadastrar Vendedor</h1>
 @stop
 
 @section('content')
+    <div class="card shadow rounded">
+        <div class="card-body">
+            <form action="{{ route('sellers.store') }}" method="post">
+                @csrf
+                @method('post')
 
+                <div class="row mb-3">
+                    <x-adminlte-input name="name" label="Nome" placeholder="Nome do Vendedor"
+                        fgroup-class="col-md-6" disable-feedback />
+                    <x-adminlte-input name="email" label="E-mail" placeholder="Email do Vendedor"
+                        fgroup-class="col-md-6" disable-feedback />
+                </div>
 
-    <form action="{{ route('sellers.store') }}" method="post">
-        @csrf
-        @method('post')
-        <div class="row">
-            <x-adminlte-input name="name" label="Nome" placeholder="Nome do Vendedor" fgroup-class="col-md-6"
-                disable-feedback />
+                <div class="row mb-3">
+                    <x-adminlte-input name="password" label="Senha" placeholder="Digite a senha" type="password"
+                        fgroup-class="col-md-6" disable-feedback />
+                    <x-adminlte-select name="selBasic" label="Perfil do Usuário" fgroup-class="col-md-6"
+                        disable-feedback>
+                        <option value="">Selecione um perfil</option>
+                        @foreach ($roles as $role)
+                            <option value="{{ $role->name }}">{{ ucfirst($role->name) }}</option>
+                        @endforeach
+                    </x-adminlte-select>
+                </div>
+
+                <div class="row">
+                    <div class="col text-end">
+                        <x-adminlte-button class="btn-flat" type="submit" label="Cadastrar"
+                            theme="success" icon="fas fa-lg fa-save" />
+                    </div>
+                </div>
+            </form>
         </div>
-
-        <div class="row">
-            <x-adminlte-input name="email" label="E-mail" placeholder="Email do vendedor" fgroup-class="col-md-6"
-                disable-feedback />
-        </div>
-
-        <div class="row">
-            <x-adminlte-input name="password" label="Senha" placeholder="Digite a senha" type="password"
-                fgroup-class="col-md-6">
-            </x-adminlte-input>
-        </div>
-
-        <div class="row">
-            <x-adminlte-select name="selBasic">
-                <option value="">Selecione um perfil</option>
-                @foreach ($roles as $role)
-                    <option value="{{ $role->name }}">{{ ucfirst($role->name) }}</option>
-                @endforeach
-            </x-adminlte-select>
-        </div>
-
-        <x-adminlte-button class="btn-flat" type="submit" label="Cadastrar" theme="success" icon="fas fa-lg fa-save" />
-    </form>
-
+    </div>
 @stop
